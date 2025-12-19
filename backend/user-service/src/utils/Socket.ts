05f4d90e99f703aba47297b5fab450588aa30c5b
AkashSurendran0 <akashsurendran.personal@gmail.com>
2025-12-19 21:12:27 +0530

    feat: Calling notifications and declining created

--- backend/user-service/src/utils/Socket.ts
+++ backend/user-service/src/utils/Socket.ts
@@ -8,9 +8,64 @@ export const initUserSocket = (server: http.Server) => {
     });
 
     const onlineUsers=new Map();
+    const busyUsers=new Set();
+    const activeCalls=new Map();
 
     io.on("connection", (socket)=>{
         console.log("User socket connected:", socket.id);
+
+        socket.on("ring-call", ({from, caller, to, reciever, callType}) => {
+            
+            if((!onlineUsers.has(to))){
+                io.to(socket.id).emit("call-failed", {
+                    reason:"USER_OFFLINE"
+                });
+                return;
+            }
+
+            if(busyUsers.has(to)){
+                io.to(socket.id).emit("call-failed", {
+                    reason:"USER_BUSY"
+                });
+                return;
+            }
+
+            if(busyUsers.has(from)){
+                io.to(socket.id).emit("call-failed", {
+                    reason:"IN_ANOTHER_CALL"
+                });
+            }
+
+            busyUsers.add(from);
+            busyUsers.add(to);
+            activeCalls.set(from, to);
+            activeCalls.set(to, from);
+
+            const receiverSocket=onlineUsers.get(to)!;
+
+            io.to(receiverSocket).emit("incoming-call", {
+                from,
+                caller,
+                callType
+            });
+
+            io.to(socket.id).emit("ringing", {
+                reciever,
+                callType
+            });
+        });
+
+        socket.on("reject-call", ({userId, callerId}) => {
+            const callerSocket=onlineUsers.get(callerId);
+            if(!callerSocket) return;
+
+            if(busyUsers.has(userId)) busyUsers.delete(userId);
+            if(busyUsers.has(callerId)) busyUsers.delete(callerId);
+            activeCalls.delete(userId);
+            activeCalls.delete(callerId);  
+
+            io.to(callerSocket).emit("call-declined");
+        });
 
         socket.on("user-online", (userId: string) => {
             onlineUsers.set(userId, socket.id);
@@ -58,6 +113,14 @@ export const initUserSocket = (server: http.Server) => {
                 const [userId] = userEntry;
                 onlineUsers.delete(userId);
 
+                if(!busyUsers.has(userId)) return;
+
+                busyUsers.delete(userId);
+                const otherUser=activeCalls.get(userId);
+                busyUsers.delete(otherUser);
+                const otherSocket=onlineUsers.get(otherUser);
+
+                io.to(otherSocket).emit("user-disconnected");
                 io.emit("online-users", Array.from(onlineUsers.keys()));
             }
         });
---
+++ frontend/src/reusable-components/callPopup.tsx
@@ -0,0 +1,50 @@
+import { Phone, PhoneOff, Camera } from "lucide-react"
+
+interface Props {
+    name:string,
+    callType:string,
+    handleRejectCall () : void
+}
+
+export default function CallPopup ({name, callType, handleRejectCall}:Props){
+
+    return  (
+        <div className="fixed top-1 left-1/2 -translate-x-1/2 mt-2 z-50 animate-in slide-in-from-top">
+            <div className="bg-white rounded-lg shadow-2xl border border-gray-200 p-4 min-w-[280px] sm:min-w-[320px]">
+              <div className="flex items-center gap-3">
+                <div className="w-10 h-10 rounded-full bg-gradient-to-br from-blue-400 to-blue-600 flex items-center justify-center flex-shrink-0">
+                    {callType == 'voice-call' ? (
+                        <Phone className="w-5 h-5 text-white animate-pulse" />
+                    ) : (
+                        <Camera className="w-5 h-5 text-white animate-pulse" />
+                    )}
+                </div>
+
+                <div className="flex-1 min-w-0">
+                  <p className="text-sm font-medium text-gray-900 truncate">{name}</p>
+                  <p className="text-xs text-gray-500">Incoming call...</p>
+                </div>
+
+                <div className="flex gap-2 flex-shrink-0">
+                  <button
+                    onClick={handleRejectCall}
+                    className="cursor-pointer flex items-center justify-center w-10 h-10 bg-red-500 hover:bg-red-600 text-white rounded-full transition-colors"
+                    title="Reject"
+                  >
+                    <PhoneOff className="w-4 h-4" />
+                  </button>
+
+                  <button
+                    // onClick={handleAcceptCall}
+                    className="cursor-pointer flex items-center justify-center w-10 h-10 bg-green-500 hover:bg-green-600 text-white rounded-full transition-colors"
+                    title="Accept"
+                  >
+                    <Phone className="w-4 h-4" />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+    )
+
+}
