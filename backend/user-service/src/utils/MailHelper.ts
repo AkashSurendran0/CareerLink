a153efcc4b02ea441daa20fa17459e5414269ee8
AkashSurendran0 <akashsurendran.personal@gmail.com>
2026-01-05 05:45:35 +0530

    fix: changed to sendgrid

--- backend/user-service/src/utils/MailHelper.ts
+++ backend/user-service/src/utils/MailHelper.ts
@@ -1,77 +1,34 @@
-import nodemailer,{Transporter} from "nodemailer";
-import {inject, injectable} from "inversify";
+import sgMail from "@sendgrid/mail";
+import { injectable } from "inversify";
 import { logger } from "./logger";
-import { google } from "googleapis";
 import dotenv from "dotenv";
 dotenv.config();
 
-const CLIENT_ID = process.env.GOOGLE_CLIENT_ID || "";
-const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET || "";
-const REDIRECT_URI = "https://developers.google.com/oauthplayground";
-const REFRESH_TOKEN = process.env.GOOGLE_REFRESH_TOKEN || "";
-const SENDER_EMAIL = process.env.SENDER_EMAIL || "akashsurendran.personal@gmail.com";
+const SENDGRID_API_KEY = process.env.SENDGRID_API || "";
+const SENDER_EMAIL = process.env.SENDGRID_EMAIL || "akashsurendran1001@gmail.com";
 
 @injectable()
 export class Mailer {
-    private _transporter?: Transporter;
-    private _oAuth2Client: any;
-    private _initializing: Promise<void> | null = null;
-
-    constructor(){
-
-        const oAuth2Client = new google.auth.OAuth2(
-            CLIENT_ID,
-            CLIENT_SECRET,
-            REDIRECT_URI
-        );
-        oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
-
-        // store client for later async initialization
-        this._oAuth2Client = oAuth2Client;
-        // start async initialization but don"t block constructor (keeps DI happy)
-        this._initializing = this.initializeTransporter();
+    constructor() {
+        sgMail.setApiKey(SENDGRID_API_KEY);
+        logger.info("SendGrid Mailer initialized successfully");
     }
 
-    private async initializeTransporter() {
-        try {
-            const tokenResponse = await this._oAuth2Client.getAccessToken();
-            const accessToken = tokenResponse?.token || "";
-
-            this._transporter = nodemailer.createTransport({
-                service: "gmail",
-                auth: {
-                    type: "OAuth2",
-                    user: SENDER_EMAIL,
-                    clientId: CLIENT_ID,
-                    clientSecret: CLIENT_SECRET,
-                    refreshToken: REFRESH_TOKEN,
-                    accessToken,
-                },
-                logger: true,
-                debug: true,
-            });
-        } catch (err) {
-            logger.error({err}, "Failed to initialize mail transporter");
-            this._transporter = undefined as unknown as Transporter;
-        }
-    }
-
-    async sendMail(to:string, subject:string, text:string){
-        const mailOptions={
+    async sendMail(to: string, subject: string, text: string) {
+        const msg = {
             from: SENDER_EMAIL,
             to,
             subject,
-            text
+            text,
         };
 
-        if (this._initializing) await this._initializing;
-
-        if (!this._transporter) {
-            throw new Error("Mail transporter not initialized");
+        try {
+            const response = await sgMail.send(msg);
+            logger.info("Mail sent");
+            return response;
+        } catch (err: any) {
+            logger.error({ err }, "Failed to send mail via SendGrid");
+            throw err;
         }
-
-        const info = await this._transporter.sendMail(mailOptions);
-        logger.info("Mail sent");
-        return info;
     }
 }
