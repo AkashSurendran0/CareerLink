c31786a4b5dbd8ed93d230e70efe2335980b9b6e
AkashSurendran0 <akashsurendran.personal@gmail.com>
2026-01-05 11:10:40 +0530

    fix: socker change

--- frontend/src/lib/socket.ts
+++ frontend/src/lib/socket.ts
@@ -2,12 +2,14 @@ import {io} from 'socket.io-client'
 
 const apiGatewayRoute=process.env.NEXT_PUBLIC_API_GATEWAY_ROUTE
 
-export const notificationSocket=io(`${apiGatewayRoute}/notification`, {
-    transports:['websocket'],
-    withCredentials:true
-})
+export const notificationSocket = io("https://careerlink.ddns.net/notification", {
+  path: "/notification/socket.io",
+  transports: ["websocket"],
+  withCredentials: true
+});;
 
-export const userSocket=io(`${apiGatewayRoute}/user`, {
-    transports:['websocket'],
-    withCredentials:true
-})
+export const userSocket = io("https://careerlink.ddns.net/user", {
+  path: "/user/socket.io",
+  transports: ["websocket"],
+  withCredentials: true
+});;
