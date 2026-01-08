69ac431e0f68bac069917340ab2855708dce3158
AkashSurendran0 <akashsurendran.personal@gmail.com>
2026-01-08 12:49:36 +0530

    fix: changed target domains

--- frontend/src/lib/socket.ts
+++ frontend/src/lib/socket.ts
@@ -2,13 +2,13 @@ import {io} from 'socket.io-client'
 
 const apiGatewayRoute=process.env.NEXT_PUBLIC_API_GATEWAY_ROUTE
 
-export const notificationSocket = io("https://careerlink.ddns.net", {
+export const notificationSocket = io("https://careerlink.space", {
   path: "/notification/socket.io",
   transports: ["websocket"],
   withCredentials: true
 });;
 
-export const userSocket = io("https://careerlink.ddns.net", {
+export const userSocket = io("https://careerlink.space", {
   path: "/user/socket.io",
   transports: ["websocket"],
   withCredentials: true
