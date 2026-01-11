44fdf3661ab482a525105d994717263f1ca4aa91
AkashSurendran0 <akashsurendran.personal@gmail.com>
2026-01-12 05:08:30 +0530

    fix: changed sequalize type

--- backend/admin-service/src/infrastructure/database/Sequelize.ts
+++ backend/admin-service/src/infrastructure/database/Sequelize.ts
@@ -1,21 +1,26 @@
 import {Sequelize} from "sequelize";
 import dotenv from "dotenv";
 import { logger } from "../../utils/logger";
-import pkg from "pg";
 
-const { Pool } = pkg;
 dotenv.config();
 
-export const sequelize=new Pool({
-    connectionString: process.env.SEQUALIZE_URL as string,
-    ssl: {
-        rejectUnauthorized: false
+export const sequelize = new Sequelize(
+    process.env.SEQUALIZE_URL as string,
+    {
+        dialect: "postgres",
+        logging: false,
+        dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+        },
     }
-});
+);
 
 export const connectDB = async () =>{ 
     try {
-        await sequelize.connect();
+        await sequelize.authenticate();
         await sequelize.query("SELECT 1");
         logger.info("Admin PSQL database connected successfully");
     } catch (error: unknown) {
--- backend/report-service/src/infrastructure/database/Sequelize.ts
+++ backend/report-service/src/infrastructure/database/Sequelize.ts
@@ -1,22 +1,26 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 import { logger } from "../../utils/logger";
-import pkg from "pg";
-
-const { Pool } = pkg;
 
 dotenv.config();
 
-export const sequelize = new Pool({
-    connectionString: process.env.SEQUALIZE_URL as string,
-    ssl: {
-        rejectUnauthorized: false
+export const sequelize = new Sequelize(
+    process.env.SEQUALIZE_URL as string,
+    {
+        dialect: "postgres",
+        logging: false,
+        dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+        },
     }
-});
+);
 
 export const connectPSQL = async () =>{
     try {
-        await sequelize.connect();
+        await sequelize.authenticate();
         await sequelize.query("SELECT 1");
         logger.info("PSQL database connected successfully!");
     } catch (error) {
