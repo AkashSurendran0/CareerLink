132d97e437d75921b7274224556835e2c0361ce1
AkashSurendran0 <akashsurendran.personal@gmail.com>
2026-01-12 12:01:28 +0530

    fix: changed routes in api-gateway

--- backend/api-gateway/src/routes/gatewayRoutes.ts
+++ backend/api-gateway/src/routes/gatewayRoutes.ts
@@ -5,15 +5,15 @@ import { authMiddleware } from "../middleware/jwtVerifier";
 const route=Router();
 const gatewayController=new GatewayController();
 
-route.use("/user", authMiddleware, gatewayController.getPath("/user"));
-route.use("/admin", authMiddleware, gatewayController.getPath("/admin"));
-route.use("/company", authMiddleware, gatewayController.getPath("/company"));
-route.use("/notification", authMiddleware, gatewayController.getPath("/notification"));
-route.use("/job", authMiddleware, gatewayController.getPath("/job"));
-route.use("/resume", authMiddleware, gatewayController.getPath("/resume"));
-route.use("/media", authMiddleware, gatewayController.getPath("/media"));
-route.use("/subscription", authMiddleware, gatewayController.getPath("/subscription"));
-route.use("/chat", authMiddleware, gatewayController.getPath("/chat"));
-route.use("/report", authMiddleware, gatewayController.getPath("/report"));
+route.use("/api/user", authMiddleware, gatewayController.getPath("/user"));
+route.use("/api/admin", authMiddleware, gatewayController.getPath("/admin"));
+route.use("/api/company", authMiddleware, gatewayController.getPath("/company"));
+route.use("/api/notification", authMiddleware, gatewayController.getPath("/notification"));
+route.use("/api/job", authMiddleware, gatewayController.getPath("/job"));
+route.use("/api/resume", authMiddleware, gatewayController.getPath("/resume"));
+route.use("/api/media", authMiddleware, gatewayController.getPath("/media"));
+route.use("/api/subscription", authMiddleware, gatewayController.getPath("/subscription"));
+route.use("/api/chat", authMiddleware, gatewayController.getPath("/chat"));
+route.use("/api/report", authMiddleware, gatewayController.getPath("/report"));
 
 export default route;
